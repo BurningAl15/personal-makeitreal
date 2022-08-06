480f8a8217f3938ddb80bfefe0c254ff4de01321
Aldhair Vera <aldhair@subterra.ai>
2022-08-05 19:49:48 -0500

    Change url for BaseUrl

--- AldhaNote/src/config/config.js
+++ AldhaNote/src/config/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://192.168.1.40:3001/api/v1';
+export const BASE_URL = 'https://aldhanote.herokuapp.com/api/v1';
+export const LOCAL_BASE_URL = 'http://192.168.1.40:3001/api/v1';
+
